bcbcc9833e1c8b5a47172b0f878b6a98bcf900c2
meds <mehdi.herzi@gmail.com>
2019-05-13 19:01:28 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,19 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Chaudiere ch1 = new Chaudiere(8);
+	    Thermometre th  = new Thermometre("alpha");
+       
+        
+        th.majTemperature();
+        
+        th.abonner(ch1);
+        
+        System.out.print(th.gettemp());
+	    
+		
+		
+		
 	}
 
 }
