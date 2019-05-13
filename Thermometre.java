42d4f7ce376a01c926a422a47e517912e6a165a4
meds <mehdi.herzi@gmail.com>
2019-05-13 13:58:54 +0200

    Update Thermometre.java

--- Thermometre.java
+++ Thermometre.java
@@ -1,10 +1,34 @@
 package iot;
 
-import  java.util.Ramdom;
+import  java.util.Random;
 
 
 
+/**
+ * @author Ordi
+ *
+ */
+public class Thermometre {     
 
-public class Thermometre {
+    private float  tempnow ;       // température actuelle 
+    private String name ;         // the name of the thermometer
+    
+    Random rdm  = new Random();
+
+    public Thermometre(String name)
+    {
+      this.tempnow = majTemperature();
+    }
+    
+    public float majTemperature()
+    {
+        return (float)rdm.nextInt(100)-50; 
+    }
+    
+ 
+    public static void  main (String [] args)
+    {
+    	System.out.print(" ");
+    }
 
 }
